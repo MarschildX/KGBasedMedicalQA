2abec574b0372e8e764d4f9bb4f88d3ddc1c38ad
MarschildX <ai.xufang@qq.com>
2021-12-05 14:45:19 +0800

    delete tmp file

--- ServerTest/WSGI/sayhello.py
+++
@@ -1,8 +0,0 @@
-# coding:utf-8
-
-def hello(environ, start_response):
-    status = "200 OK"
-    response_headers = [('Content-Type', 'text/html')]
-    start_response(status, response_headers)
-    path = environ['PATH_INFO'][1:] or 'hello'
-    return [b'<h1> %s </h1>' % path.encode()]
